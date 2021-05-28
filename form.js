56ee1c5cb9b84035ea06f3e1230272c33812ab12
thegauravantil <gauravsatila@gmail.com>
2021-05-28 16:16:21 +0530

    files added via upload

---
+++ form.js
@@ -0,0 +1,47 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDQMOUcFke7M3SfbT22RGkUtj7VHg22J7U",
+    authDomain: "task-45546.firebaseapp.com",
+    projectId: "task-45546",
+    storageBucket: "task-45546.appspot.com",
+    messagingSenderId: "102610638236",
+    appId: "1:102610638236:web:30200d8e79666e0cb46459"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+
+  function signup() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+
+    alert("Signed up");
+  }
+
+  function signin() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.catch(e=>alert(e.message));
+
+    //alert("Signed in"+email.value);
+  }
+
+  function signout(){
+      auth.signOut();
+      alert("Signed out");
+  }
+
+  auth.onAuthStateChanged(function(user){
+      if(user){
+        var email = user.email;
+        alert("active user" + email.value);
+      }
+      else{
+          alert("no active user");
+
+      }
+  })
